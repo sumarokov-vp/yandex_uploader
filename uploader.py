31552941c2ce57b6b52bceb1f8d9076021466572
sumarokov-vp <sumarokov.vp@gmail.com>
2023-11-01 02:14:44 +0600

    logging

--- uploader.py
+++ uploader.py
@@ -15,26 +15,26 @@ from yandex_disk import YDWorker
 SETTINGS = os.path.join(os.path.dirname(__file__), "settings.json")
 
 
-def main():
-    log = LogWorker(DEBUG)
+def main(logger=None):
     # load local folders list from settings.json
     with open(SETTINGS, "r") as f:
         settings = json.load(f)
     folders = settings["yandex"]["local_folders"]
 
     # # upload files from local folders to Yandex.Disk
-    log.debug("Creating Yandex.Disk worker")
-    yandex_worker = YDWorker(log)
-    log.debug("Yandex.Disk worker created")
+    logger.debug("Creating Yandex.Disk worker")
+    yandex_worker = YDWorker(logger=logger)
+    logger.debug("Yandex.Disk worker created")
     for folder in folders:
         full_path = os.path.join(os.path.dirname(__file__), folder)
         yandex_worker.recursive_upload(full_path)
 
 
 def loop():
+    log = LogWorker(DEBUG)
     while True:
-        main()
-        time.sleep(3)
+        main(log)
+        time.sleep(10)
 
 
 if __name__ == "__main__":
