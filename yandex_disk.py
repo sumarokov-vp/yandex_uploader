26aa013f9dc8c9076604f0145fba5ffd1b92edd4
sumarokov-vp <sumarokov.vp@gmail.com>
2023-11-01 21:24:58 +0600

    7

--- yandex_disk.py
+++ yandex_disk.py
@@ -5,6 +5,10 @@ import os
 import posixpath
 import uuid
 import webbrowser
+from pathlib import (
+    PurePosixPath,
+    PureWindowsPath,
+)
 
 # Third Party Stuff
 import pytz
@@ -89,9 +93,10 @@ class YDWorker:
                 path = os.path.join(root, file)
 
                 dest_file_path = posixpath.join(dest_folder, file)
-                dest_file_path_true = posixpath.join(dest_folder, p, file)
+                p_posix = PurePosixPath(p)
+                dest_file_path_true = posixpath.join(dest_folder, p_posix, file)
                 self.l.debug(
-                    f"!!! creating dest_file_path from {dest_folder=} and {file=} = {dest_file_path=} \n{p=} {root=} {path=} {dest_file_path_true=}"
+                    f"!!! creating dest_file_path from {dest_folder=} and {file=} = {dest_file_path=} \n{p=} {p_posix=} {root=} {path=} {dest_file_path_true=}"
                 )
                 p_sys = p.replace("/", os.path.sep)
                 source_file_path = os.path.join(from_dir, p_sys, file)
